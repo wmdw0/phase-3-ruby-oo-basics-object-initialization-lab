a059c13e5953fdf67da3c94beb29fcab08c35e71
William Walsh <wmdwalsh0@gmail.com>
2021-10-03 11:31:39 -0400

    completed assignment

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+    attr_accessor :name, :breed
+    def initialize(name, breed="Mutt")
+      puts @name = name
+      puts @breed = breed
+    end
+  end 
+    # def get_name(name)
+    #   @name
+    # end
+
+    # def breed=(breed)
+    #     @breed = breed
+    #   end
+    
+    #   def breed
+    #     @breed
+    #   end
+      
+  # end
+ 
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+    attr_accessor :name
+    def initialize(name)
+        puts @name = name
+    end
+end
